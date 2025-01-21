6ba14a5a88fbcbb111756793bd19f4ab317b3ee5
Aaditya-brrt <denduluriaaditya@gmail.com>
2025-01-20 22:06:49 -0500

    update elevator sim

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -7,8 +7,8 @@ public class ElevatorConstants {
 
   public static final DCMotor m_elevatorMotors = DCMotor.getNEO(2);
   public static final double kElevatorGearing = 6;
-  public static final double kCarriageMassKg = Units.lbsToKilograms(20);
-  public static final double kElevatorDrumRadiusMeters = Units.inchesToMeters(1.625 / 2);
+  public static final double kCarriageMassKg = Units.lbsToKilograms(16.142);
+  public static final double kElevatorDrumRadiusMeters = Units.inchesToMeters(0.829 / 2);
   public static final double kMinElevatorHeightMeters = 0;
-  public static final double kMaxElevatorHeightMeters = Units.inchesToMeters(80);
+  public static final double kMaxElevatorHeightMeters = Units.inchesToMeters(59.035);
 }
