04b7bc6b5c58b3420fb3e6269107dff06d522131
Adele <mcshanealincoln@gmail.com>
2025-02-18 19:02:03 -0500

    hopefully fixed d to hp path

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-main";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "5dae5f0528782f7ea95c3f548de52a883008a7ac";
-  public static final String GIT_DATE = "2025-02-15 15:23:56 EST";
+  public static final int GIT_REVISION = 30;
+  public static final String GIT_SHA = "5787255aba4e8ed8de59998981470dc094f90800";
+  public static final String GIT_DATE = "2025-02-18 18:55:37 EST";
   public static final String GIT_BRANCH = "autos_pt2";
-  public static final String BUILD_DATE = "2025-02-18 18:42:11 EST";
-  public static final long BUILD_UNIX_TIME = 1739922131770L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-18 18:56:27 EST";
+  public static final long BUILD_UNIX_TIME = 1739922987938L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
