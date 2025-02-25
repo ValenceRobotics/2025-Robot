09212eeeed361bad2c4fd311ad2f9505ee957fad
Valence <valencerobotics@gmail.com>
2025-02-24 19:17:38 -0500

    hi

---
+++ 2025-Robot-main/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME =
+      "AdvantageKit_SparkSwerveTemplate-maple-sim copy-Imported";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "a39239a3a897c73e41516c901a48ddfb08342c61";
+  public static final String GIT_DATE = "2025-02-16 01:47:33 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-02-22 16:06:01 EST";
+  public static final long BUILD_UNIX_TIME = 1740258361116L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
