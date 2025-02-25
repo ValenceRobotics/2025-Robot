c56f6e730e5839f65eb4a2124447c110c9ea5069
Adele <mcshanealincoln@gmail.com>
2025-02-24 20:33:22 -0500

    speeding l2 a reasonable amounbt

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-main";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 46;
-  public static final String GIT_SHA = "621ccbdd1f5b980e3a7bfb5574672e8844bf39f7";
-  public static final String GIT_DATE = "2025-02-24 19:37:54 EST";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "9ac9be8da2f64673423b9de3bafbad1f0e3fb4c9";
+  public static final String GIT_DATE = "2025-02-24 20:29:24 EST";
   public static final String GIT_BRANCH = "autos_pt2";
-  public static final String BUILD_DATE = "2025-02-24 20:04:43 EST";
-  public static final long BUILD_UNIX_TIME = 1740445483097L;
+  public static final String BUILD_DATE = "2025-02-24 20:31:12 EST";
+  public static final long BUILD_UNIX_TIME = 1740447072265L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/EndEffector/EndEffectorIOReal.java
+++ src/main/java/frc/robot/subsystems/EndEffector/EndEffectorIOReal.java
@@ -70,7 +70,7 @@ public class EndEffectorIOReal implements EndEffectorIO {
           // setVoltage(3);
         } else if (RobotState.getCurrentElevatorState() == ElevatorState.L2
             || RobotState.getCurrentElevatorState() == ElevatorState.L3) {
-          setVoltage(8);
+          setVoltage(6.5);
         } else {
           setVoltage(3);
         }
