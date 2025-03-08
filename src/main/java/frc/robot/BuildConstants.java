ae43bc8953241434b60592abe89ea4d6dc1c1618
Adele <mcshanealincoln@gmail.com>
2025-03-08 12:15:41 -0500

    merge autos

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,9 +3,11 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME =
-      "AdvantageKit_SparkSwerveTemplate-maple-sim copy-Imported";
+  public static final String MAVEN_NAME = "2025-Robot-main";
   public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "c8d51549b72d468b65d5dffefb20ee550a960cf5";
+  public static final String GIT_DATE = "2025-03-03 21:34:38 EST";
   public static final int GIT_REVISION = 63;
   public static final String GIT_SHA = "c8d51549b72d468b65d5dffefb20ee550a960cf5";
   public static final String GIT_DATE = "2025-03-03 21:34:38 EST";
