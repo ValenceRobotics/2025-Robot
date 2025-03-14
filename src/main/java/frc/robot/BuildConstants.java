36a2d9e628f0cdd7cf66621824c8cf54b2f2a7a7
Adele <mcshanealincoln@gmail.com>
2025-03-14 13:29:12 -0400

    path fixies

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME =
-      "AdvantageKit_SparkSwerveTemplate-maple-sim copy-Imported";
+  public static final String MAVEN_NAME = "2025-Robot-main";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "9428c9a94fd4b49fd3a32cf5fc920143da05be78";
-  public static final String GIT_DATE = "2025-03-14 01:36:26 EDT";
+  public static final int GIT_REVISION = 74;
+  public static final String GIT_SHA = "f723223f88e029078ffa0c9e176125f54c2a2ffa";
+  public static final String GIT_DATE = "2025-03-14 02:11:00 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-14 02:03:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1741932239994L;
+  public static final String BUILD_DATE = "2025-03-14 13:25:11 EDT";
+  public static final long BUILD_UNIX_TIME = 1741973111420L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
