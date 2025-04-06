2c6a58dfd76df709c965ead0351d479ad485b5c1
Aaditya-brrt <denduluriaaditya@gmail.com>
2025-04-06 08:51:37 -0400

    pre l1 change

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,12 +6,12 @@ public final class BuildConstants {
   public static final String MAVEN_NAME =
       "AdvantageKit_SparkSwerveTemplate-maple-sim copy-Imported";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 105;
-  public static final String GIT_SHA = "6e7b82089f29bfa17c56e2b976d33e104b3c8386";
-  public static final String GIT_DATE = "2025-04-05 18:05:37 EDT";
+  public static final int GIT_REVISION = 106;
+  public static final String GIT_SHA = "779d00fd5e0daddde332165be90b481937b774d1";
+  public static final String GIT_DATE = "2025-04-05 23:01:53 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-05 18:32:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1743892365409L;
+  public static final String BUILD_DATE = "2025-04-06 08:41:52 EDT";
+  public static final long BUILD_UNIX_TIME = 1743943312418L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
